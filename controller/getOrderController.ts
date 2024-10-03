327285a1921f8a237d69b24b284f8b3da5a8d1b9
Your Name <you@example.com>
2024-10-03 10:17:35 +0200

    yoco400-scheme-commit

--- controller/getOrderController.ts
+++ controller/getOrderController.ts
@@ -3,12 +3,11 @@ import { orderModel } from '../model/order_schema'; // Import your order model
 import { IOrder } from '../interface&Objects/IOrder'; // Import the interfaces
 
 const GetOrderController = async (req: Request, res: Response) => {
-  const { } = req.body; // Get the orderId from the request parameters
 
   const userId =  req.session.userData?.userID;
 
 
-  console.log(`User ID ${userId}`);
+  console.log(`User Order ID ${userId}`);
   try {
     // Find the order where 'checkOutObject.orderId' matches the passed orderId
     const order: Array<IOrder> | null = await orderModel.find({ 'checkOutObject.userId': userId }).exec();
--- dist/controller/getOrderController.js
+++ dist/controller/getOrderController.js
@@ -12,9 +12,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const order_schema_1 = require("../model/order_schema"); // Import your order model
 const GetOrderController = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
-    const {} = req.body; // Get the orderId from the request parameters
     const userId = (_a = req.session.userData) === null || _a === void 0 ? void 0 : _a.userID;
-    console.log(`User ID ${userId}`);
+    console.log(`User Order ID ${userId}`);
     try {
         // Find the order where 'checkOutObject.orderId' matches the passed orderId
         const order = yield order_schema_1.orderModel.find({ 'checkOutObject.userId': userId }).exec();
