284d0828670343c281eab840e4739c2d15e20cd5
Your Name <you@example.com>
2024-10-10 19:53:30 +0200

    yoco47-scheme-commit

--- controller/signUpController.ts
+++ controller/signUpController.ts
@@ -2,7 +2,23 @@
 import { Request, Response} from 'express';
 import bcrypt from 'bcrypt';
 import { userModel } from '../model/user_schema';
+import nodemailer, { TransportOptions , SentMessageInfo} from 'nodemailer';
 
+const emailHost: string | undefined = process.env.EMAIL_HOST;
+const emailPort = process.env.EMAIL_PORT;
+const emailHostUser = process.env.EMAIL_HOST_USER;
+const emailHostPassword = process.env.EMAIL_HOST_PASSWORD;
+
+// Setup email transporter
+const transporter = nodemailer.createTransport({
+  host: emailHost,
+  port: +emailPort!,
+  secure: false, 
+  auth: {
+    user: emailHostUser,
+    pass: emailHostPassword,
+  },
+} as TransportOptions);
 
 //================================function for handling signing up=======================================
 //==================================================================================================
@@ -41,6 +57,7 @@ const SignUpController = async ( request : Request , response : Response) => {
             await user.save();
 
             console.log('Data saved into database');
+            sendWelcomeEmail(name,email);
             response.status(200).send({ message: 'User saved into the database' });
 
         } catch (error) {
@@ -55,6 +72,49 @@ const SignUpController = async ( request : Request , response : Response) => {
                 }
         }
 }
+
+
+
+
+const sendWelcomeEmail = (userName: string, email: string): void => {
+    const welcomeHtml = `
+    <div style="font-family: Arial, sans-serif; color: #333;">
+      <div style="text-align: center;">
+        <img src="https://i.imgur.com/PA5VTwK.png" alt="Scentor Logo" style="width: 100px; height: auto;" />
+      </div>
+      <h2>Welcome to Alkebulan, ${userName}! 🎉</h2>
+      <p>We're thrilled to have you onboard in our little corner of the internet! Whether you're here for a new wardrobe, the latest tech, or that quirky gadget you never knew you needed, we’ve got it all.</p>
+      
+      <h3>Here's what you can expect:</h3>
+      <ul>
+        <li>Hot deals served fresh!</li>
+        <li>Curated picks just for you (we like to think we're pretty good matchmakers).</li>
+        <li>Super speedy shipping 🚀!</li>
+      </ul>
+  
+      <p>So, go ahead and start filling that cart. We promise, your shopping journey with us will be smoother than a new pair of silk socks.</p>
+  
+      <p>Got questions? We’re always here for you – just drop us a line.</p>
+  
+      <p>Happy shopping!<br/>
+      The Alkebulan Ya Batho Team 😊</p>
+    </div>
+    `;
+  
+    transporter.sendMail({
+      from: 'Alkebulan <alkebulanyabatho@gmail.com>',
+      to: `${email}`,
+      subject: 'Welcome to Alkebulan – Let the Shopping Begin! 🛒',
+      html: welcomeHtml,
+    }, (error: Error | null, info: SentMessageInfo) => {
+      if (error) {
+        console.error(`Error sending email: ${error.message}`);
+      } else {
+        console.log(`Welcome email sent: ${info.response}`);
+      }
+    });
+  };
+  
 
 export default SignUpController;
 
--- dist/controller/signUpController.js
+++ dist/controller/signUpController.js
@@ -14,6 +14,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const bcrypt_1 = __importDefault(require("bcrypt"));
 const user_schema_1 = require("../model/user_schema");
+const nodemailer_1 = __importDefault(require("nodemailer"));
+const emailHost = process.env.EMAIL_HOST;
+const emailPort = process.env.EMAIL_PORT;
+const emailHostUser = process.env.EMAIL_HOST_USER;
+const emailHostPassword = process.env.EMAIL_HOST_PASSWORD;
+// Setup email transporter
+const transporter = nodemailer_1.default.createTransport({
+    host: emailHost,
+    port: +emailPort,
+    secure: false,
+    auth: {
+        user: emailHostUser,
+        pass: emailHostPassword,
+    },
+});
 //================================function for handling signing up=======================================
 //==================================================================================================
 const SignUpController = (request, response) => __awaiter(void 0, void 0, void 0, function* () {
@@ -45,6 +60,7 @@ const SignUpController = (request, response) => __awaiter(void 0, void 0, void 0
         // Save data into a database
         yield user.save();
         console.log('Data saved into database');
+        sendWelcomeEmail(name);
         response.status(200).send({ message: 'User saved into the database' });
     }
     catch (error) {
@@ -58,4 +74,42 @@ const SignUpController = (request, response) => __awaiter(void 0, void 0, void 0
         }
     }
 });
+const sendWelcomeEmail = (userName, email) => {
+    const welcomeHtml = `
+    <div style="font-family: Arial, sans-serif; color: #333;">
+      <div style="text-align: center;">
+        <img src="https://i.imgur.com/PA5VTwK.png" alt="Scentor Logo" style="width: 100px; height: auto;" />
+      </div>
+      <h2>Welcome to Alkebulan, ${userName}! 🎉</h2>
+      <p>We're thrilled to have you onboard in our little corner of the internet! Whether you're here for a new wardrobe, the latest tech, or that quirky gadget you never knew you needed, we’ve got it all.</p>
+      
+      <h3>Here's what you can expect:</h3>
+      <ul>
+        <li>Hot deals served fresh!</li>
+        <li>Curated picks just for you (we like to think we're pretty good matchmakers).</li>
+        <li>Super speedy shipping 🚀!</li>
+      </ul>
+  
+      <p>So, go ahead and start filling that cart. We promise, your shopping journey with us will be smoother than a new pair of silk socks.</p>
+  
+      <p>Got questions? We’re always here for you – just drop us a line.</p>
+  
+      <p>Happy shopping!<br/>
+      The Alkebulan Ya Batho Team 😊</p>
+    </div>
+    `;
+    transporter.sendMail({
+        from: 'Alkebulan <alkebulanyabatho@gmail.com>',
+        to: `${email}`,
+        subject: 'Welcome to Alkebulan – Let the Shopping Begin! 🛒',
+        html: welcomeHtml,
+    }, (error, info) => {
+        if (error) {
+            console.error(`Error sending email: ${error.message}`);
+        }
+        else {
+            console.log(`Welcome email sent: ${info.response}`);
+        }
+    });
+};
 exports.default = SignUpController;
